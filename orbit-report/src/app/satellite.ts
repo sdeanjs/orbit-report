8712a4d4c6c9782d6121accba046acf4a5048d8b
sdeanjs <sdeanjs@gmail.com>
2019-12-18 19:10:26 -0600

    assignment completed

---
+++ orbit-report/src/app/satellite.ts
@@ -0,0 +1,30 @@
+
+export class Satellite {
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;      
+    
+    shouldShowWarning(){
+        if (this.type.toLowerCase() === "space debris"){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+        this.name = name;
+        this.orbitType = orbitType;
+        this.type = type;
+        this.operational = operational;
+        this.launchDate = launchDate;
+    }
+
+    
+}
+
+
+
+
